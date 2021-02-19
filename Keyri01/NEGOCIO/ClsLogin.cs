3e2e732c6c41bc90124021ef320ebe3132e584dd
Keyri2020 <70116371+Keyri2020@users.noreply.github.com>
2021-02-19 14:54:42 -0600

    Proyecto1

---
+++ Keyri01/NEGOCIO/ClsLogin.cs
@@ -0,0 +1,14 @@
+﻿using Keyri01.DOMINIO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Keyri01.NEGOCIO
+{
+    class ClsLogin
+    {
+        Login log = new Login();
+    }
+}
